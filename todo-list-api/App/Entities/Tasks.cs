ad4b61471995d39586253956c34bd897c62e6fde
Werik Santos <werik.msti@gmail.com>
2022-02-24 22:15:38 -0300

    initial commits

---
+++ todo-list-api/App/Entities/Tasks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todo_list_api.App.Entities
+{
+    public class Tasks
+    {
+        public int IdList { get; set; }
+        public int IdTask { get; set; }
+        public string Task { get; set; }
+        public bool Done { get; set; }
+        public DateTime DateSave { get; set; }
+        public DateTime DateUpdate { get; set; }
+
+    }
+}
---
+++ todo-list-api/Controllers/todolistController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using todo_list_api.App.Entities;
+
+namespace todo_list_api.Controllers
+{
+    public class TodolistController : Controller
+    {
+        // GET: todolistController
+        public string MyTasks(int idList)
+        {
+            // var myTodo = new Tasks();
+            var a = 'taks';
+            return a
+           
+        }
+
+        // GET: todolistController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: todolistController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: todolistController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: todolistController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: todolistController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: todolistController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: todolistController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
